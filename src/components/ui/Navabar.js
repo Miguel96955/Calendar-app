695b5b40003988eab0296ef2aae84d8a8919167e
Miguel Vega <miguel.co.2003@gmail.com>
2021-05-18 15:12:44 -0500

    first commit

---
+++ src/components/ui/Navabar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Navabar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-barnd">
+                Pedro
+            </span>
+             
+            <button className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+               <span> Salir</span>
+            </button>
+
+        </div>
+    )
+}
+ 
+
